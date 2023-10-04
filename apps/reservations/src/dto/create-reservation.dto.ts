82799f339afa1cbca66890edec8e1c4c5c896035
Yaroslav Maksymchuk <yaroslav.maksymchuk@gmail.com>
2023-10-04 12:20:12 +0300

    Add reservations microservice

---
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -0,0 +1,21 @@
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsDate, IsString } from 'class-validator';
+
+export class CreateReservationDto {
+
+  @IsDate()
+  @Type(() => Date)
+  startDate: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
+  placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  invoiceId: string;
+}
